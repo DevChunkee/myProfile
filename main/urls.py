17a453f3988d4dd38cf88721d326f5d478cb0540
Chunkee123 <127171820+DevChunkee@users.noreply.github.com>
2024-04-07 13:44:40 +0400

    created website's architecture

--- main/urls.py
+++ main/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('about', views.about, name='about'),
+    path('experience', views.experience, name='experiece'),
+    path('projects', views.projects, name='projects'),
+    path('contacts', views.contacts, name='contacts'),
 ]
--- main/views.py
+++ main/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'main/home.html')
+
+def about(request):
+    return render(request, 'main/about.html')
+
+def experience(request):
+    return render(request, 'main/experience.html')
+
+def projects(request):
+    return render(request, 'main/projects.html')
+
+def contacts(request):
+    return render(request, 'main/contacts.html')
